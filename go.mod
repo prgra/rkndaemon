77cd20e0dd44850c278c525cb76762b3e0f56010
prgr <me@prgr.org>
2022-02-16 22:38:02 +0300

    dns resolver начало

--- go.mod
+++ go.mod
@@ -7,3 +7,12 @@ require (
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd
 	golang.org/x/text v0.3.7
 )
+
+require (
+	github.com/bogdanovich/dns_resolver v0.0.0-20170211073258-a8e42bc6a5b6 // indirect
+	github.com/miekg/dns v1.1.46 // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/tools v0.1.6-0.20210726203631-07bc1bf47fb2 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
---
+++ resolver/resolver.go
@@ -0,0 +1,73 @@
+package resolver
+
+import (
+	"log"
+	"net"
+	"net/url"
+	"sync"
+
+	"github.com/bogdanovich/dns_resolver"
+)
+
+type Resolver struct {
+	inChan      chan url.URL
+	outChan     chan []net.IP
+	waitGroup   *sync.WaitGroup
+	dnsResolver *dns_resolver.DnsResolver
+}
+
+func New(dnsservers []string) *Resolver {
+	dnsresolver := dns_resolver.New(dnsservers)
+	dnsresolver.RetryTimes = 3
+	return &Resolver{
+		inChan:      make(chan url.URL, 1000),
+		outChan:     make(chan []net.IP),
+		dnsResolver: dnsresolver,
+	}
+}
+
+func (r Resolver) AddToQueue(url url.URL) {
+	r.inChan <- url
+}
+
+func (r Resolver) worker() {
+	for {
+		dom, ok := <-r.inChan
+		if !ok {
+			r.waitGroup.Done()
+			return
+		}
+		ips, err := r.dnsResolver.LookupHost(dom.Hostname())
+		if err != nil {
+			log.Println("LookupHost", err)
+
+		}
+		ipsmap := make(map[string]bool)
+		for i := range ips {
+			if ips[i].To4() != nil {
+				ipsmap[ips[i].String()] = true
+			}
+		}
+
+		ips2, err := net.LookupHost(dom.Hostname())
+		if err != nil {
+			log.Println("net.LookupHost", err)
+		}
+
+		for i := range ips2 {
+			a := net.ParseIP(ips2[i])
+			if a.To4() != nil {
+				ipsmap[ips[i].String()] = true
+			}
+		}
+		var res []net.IP
+		for k := range ipsmap {
+			ip := net.ParseIP(k)
+			if ip.To4() != nil {
+				res = append(res, ip)
+			}
+		}
+
+		r.outChan <- res
+	}
+}
